87db1b5d4f164984ee85062747a6b8575735ecbb
thang0409 <ncthang247@gmail.com>
2025-06-25 07:46:02 +0700

    Update Style Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import '@styles/main.scss';
 import MainLayout from '@components/Layout/Layout';
-import MyHeader from '@components/Header/Header';
 import MyFooter from '@components/Footer/Footer';
+import MyHeader from '@components/Header/Header';
 
 function App() {
     return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,50 +8,63 @@ import heartIcon from '@icons/svgs/heart.svg';
 import cartIcon from '@icons/svgs/cart_icon.svg';
 
 function MyHeader() {
-    const { containerBox, containerBoxIcon, containerMenu, containerHeader } =
-        styles;
+    const {
+        containerBox,
+        containerBoxIcon,
+        containerMenu,
+        containerHeader,
+        container
+    } = styles;
     return (
-        <div className={containerHeader}>
-            {/* Khối bên trái */}
+        <div className={container}>
+            <div className={containerHeader}>
+                {/* Khối bên trái */}
 
-            <div className={containerBox}>
-                <div className={containerBoxIcon}>
-                    {dataBoxIcon.map((item) => {
-                        return <BoxIcon type={item.type} href={item.href} />;
-                    })}
-                </div>
-                <div className={containerMenu}>
-                    {dataMenu.slice(0, 3).map((item) => {
-                        return <Menu content={item.content} href={item.href} />;
-                    })}
-                </div>
-            </div>
-
-            {/* Khối giữa */}
-
-            <div>
-                <img
-                    src={Logo}
-                    alt='logo'
-                    style={{
-                        width: '153px',
-                        height: '53px'
-                    }}
-                />
-            </div>
-
-            {/* Khối bên phải */}
-            <div className={containerBox}>
-                <div className={containerMenu}>
-                    {dataMenu.slice(3, dataMenu.length).map((item) => {
-                        return <Menu content={item.content} href={item.href} />;
-                    })}
+                <div className={containerBox}>
+                    <div className={containerBoxIcon}>
+                        {dataBoxIcon.map((item) => {
+                            return (
+                                <BoxIcon type={item.type} href={item.href} />
+                            );
+                        })}
+                    </div>
+                    <div className={containerMenu}>
+                        {dataMenu.slice(0, 3).map((item) => {
+                            return (
+                                <Menu content={item.content} href={item.href} />
+                            );
+                        })}
+                    </div>
                 </div>
 
-                <div className={containerBoxIcon}>
-                    <img width={26} height={26} src={reloadIcon} alt='' />
-                    <img width={26} height={26} src={heartIcon} alt='' />
-                    <img width={26} height={26} src={cartIcon} alt='' />
+                {/* Khối giữa */}
+
+                <div>
+                    <img
+                        src={Logo}
+                        alt='logo'
+                        style={{
+                            width: '153px',
+                            height: '53px'
+                        }}
+                    />
+                </div>
+
+                {/* Khối bên phải */}
+                <div className={containerBox}>
+                    <div className={containerMenu}>
+                        {dataMenu.slice(3, dataMenu.length).map((item) => {
+                            return (
+                                <Menu content={item.content} href={item.href} />
+                            );
+                        })}
+                    </div>
+
+                    <div className={containerBoxIcon}>
+                        <img width={26} height={26} src={reloadIcon} alt='' />
+                        <img width={26} height={26} src={heartIcon} alt='' />
+                        <img width={26} height={26} src={cartIcon} alt='' />
+                    </div>
                 </div>
             </div>
         </div>
